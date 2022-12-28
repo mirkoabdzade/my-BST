ee680c6150b3b641a69e8bfd336da77905792e6e
Mirko Abdzadeh <79837445+mirkoabdzade@users.noreply.github.com>
2022-12-28 12:16:31 +0100

    My first commit

---
+++ index.js
@@ -0,0 +1,58 @@
+function myFunction() {
+  document.getElementById("demo").innerHTML = "\n" +
+    "Gehen Sie einfach zur Menüleiste und suchen Sie das Shop-Symbol und sehen Sie sofort alle unsere Produkte und wertvollen Artikel. Kaufen Sie sie einfach, wie ein Glas Wasser zu trinken, und wir versenden sofort an Ihre Adresse, einfacher gibt es nicht ...";
+}
+
+function safranFunction() {
+  window.open("https://de.wikipedia.org/wiki/Safran");
+}
+
+function truffelFunction() {
+  window.open("https://de.wikipedia.org/wiki/Tr%C3%BCffel");
+}
+
+function berberitzenFunction() {
+  window.open("https://de.wikipedia.org/wiki/Berberitzen");
+}
+
+
+function mehrSafranFunction() {
+  document.getElementById("mehrSafran_demo").innerHTML = "\n" +
+    "Die wichtigste Methode zur Qualitätsbestimmung von Safran\n" +
+    "Der erste Faktor und vielleicht der wichtigste Faktor, um die Qualität von Safran zu bestimmen, ist der Trompetenkopf wie Safran. In jedem Safranzweig können Sie leicht erkennen, dass der Kopf jedes Safranzweigs eine trompetenartige Form hat, und je größer dieser Trompetenteil ist, desto höher ist die Farbkraft dieses Safrans.\n" +
+    "\n" +
+    "Farbstärke ist für Menschen sehr wichtig, aber nicht jede Farbe ist gut. Die künstliche Farbe des Safrans färbt das Wasser orange. Während die eigentliche Farbe des Safrans im ersten Wasser gelb sein sollte, wird das Wasser dann orange. Trompete wie Safran ist sehr wichtig für den Export. Exportierter Safran muss diese Eigenschaft aufweisen. Trockenheit von Safran\n" +
+    "Der Safranzweig ist zerbrechlich. Der zweite wirksame Faktor bei der Bestimmung der Safranqualität ist seine Trockenheit. Der Safranzweig sollte nicht geknickt werden. Wenn Sie es auf Ihre Zähne auftragen, sollten Sie feststellen, dass der Safranzweig bricht. Wenn der Safran weich und flexibel ist, ist dieser Safran überhaupt kein guter Safran und seine Färbekraft ist gering. Die Länge jedes Safranzweigs\n" +
+    "Je länger der Safranzweig, desto höher sein Wert. Vielleicht wird diese Eigenschaft bei vielen Safranen, die für den persönlichen Verzehr verwendet werden sollen, nicht beobachtet und ist nicht wichtig. Aber der Safran, der diese Eigenschaft hat, ist sehr gut. Anders verhält es sich bei Masseneinkäufen und Ausfuhren. Eines der Merkmale von exportiertem Safran ist, dass er lang und nicht weiß ist. Geschmeidigkeit von Safran\n" +
+    "Sie werden es vielleicht nicht glauben, aber selbst die Geschmeidigkeit des Safrans wirkt sich auf seinen Preis aus. Ob Safran wirklich glatt ist oder nicht, hat keinen Einfluss auf seine Qualität, aber auf seinen Preis. Dieses Kriterium sollte für eine Familie, die selbst Safran verwenden möchte, überhaupt keine Rolle spielen, ist aber für den Export von Safran sehr wichtig.\n" +
+    "\n" +
+    "Safranfarbe\n" +
+    "Je heller der Safran, desto höher sein Wert. Die dunkle Farbe des Safrans (eine Farbe, die Schwarz ähnelt) kann bedeuten, dass der Safran alt oder spät gesammelt ist. Die leuchtende Farbe des Safrans ist eine Möglichkeit, die Qualität des exportierten Safrans zu identifizieren.\n" +
+    "\n" +
+    "Wenn Sie Safran für den Export kaufen möchten, stellen Sie sicher, dass seine Farbe hell ist und kaufen Sie den diesjährigen Safran. Natürlich können Sie in seinem Testblatt erfahren, dass es sich um dieses Jahr und die Qualität von Safran handelt."
+}
+
+function mehrTruffelFunction() {
+  document.getElementById("mehrTruffel_demo").innerHTML = "\n" +
+    "Wer die Sorten und Erntezeiten der Trüffel nicht genau kennt, könnte sie auf den ersten Blick mit einer Sommertrüffel oder Scorzone verwechseln: Begünstigt wird dieser Irrtum auch durch den warzigen äußeren Teil (das Peridium), ähnlich dem der Trüffel Sommertrüffel, aber mit Warzen, die kleiner und akzentuierter sind als die von Scorzone. Wenn wir uns jedoch konzentrieren und es gut unter dem Mikroskop beobachten, werden wir einige einzigartige Eigenschaften entdecken. Wir sprechen von vielen kleinen hakenförmigen Kämmen auf den Sporen des Trüffels: Diese geben dieser besonderen Sorte den Namen und lassen uns den Hakentrüffel erkennen.\n" +
+    "\n" +
+    " \n" +
+    "\n" +
+    "Zur weiteren Unterscheidung von der Scorzone greift auch die dunklere Farbe der Gleba (der innere Teil) ein, die zur Schokoladenfarbe neigt und im reifen Zustand verzweigte weißliche Adern aufweist. Neben der Form, die sowohl abgerundet als auch abgeflacht sein kann, ist es auch der Preis, der diese besondere Sorte ausmacht: Er liegt sogar über dem der Scorzone. Dies liegt daran, dass die Reifezeit des Hakentrüffels kürzer ist als die des Scorzone: Dies beeinflusst zusammen mit anderen Faktoren (z. B. klimatischen Bedingungen) zwangsläufig seinen Preis.\n" +
+    "\n" +
+    " \n" +
+    "\n" +
+    "Achten Sie jedoch darauf, den Hakentrüffel nicht mit einem anderen sehr ähnlichen Trüffel zu verwechseln, der zur gleichen Zeit, auf derselben Höhe und oft auch in demselben Lebensraum vorkommt: Wir sprechen von der Tuber bellonae, die im gesamten Apennin verbreitet ist , aber mehr in der Mitte-Süden präsent.\n" +
+    "\n" +
+    "Von einigen mit der Tuber mesentericum verwechselt, von der sie eine \"Cousine\" ist (und mit der sie den starken Geruch teilt und für einige Mottenkugeln ähnelt, für andere Benzin), hat die Tuber bellonae eine Peridie, die der der Hakentrüffel , aber mit einer kaum wahrnehmbaren rötlichen Reflektion, die manchmal sogar den Experten entgeht; Angesichts dieser ähnlichen Eigenschaften ist der beste Weg, es zu erkennen, eine kleine Schnitzerei zu verwenden, um die Gleba zu kontrollieren, die braun ist, aber amarant / violette Reflexe hat."
+}
+
+function mehrBerberitzenFunction() {
+  document.getElementById("mehrBerberitzen_demo").innerHTML = "\n" +
+    "Wie sieht Farbe und Aussehen einer guten Berberitze aus?, \n" +
+    "Wie schmeckt eine gute Berberitze?, \n" +
+    "Wie ist ein gutes Berberitzenparfüm?, \n" +
+    "Wie ist die Konsistenz und Gleichmäßigkeit einer guten Berberitze? Sie haben gelernt, darauf zu achten, auf welche Elemente und Faktoren Sie hochwertige Berberitze kaufen. Saure und leckere Berberitze kann Ihr Essen schmackhafter machen und Ihrem Körper natürlich viele Nährstoffe zuführen. Berberitze ist reich an Vitaminen und Antioxidantien und kann die Widerstandskraft des Körpers gegen Bakterien und Viren erhöhen.\n" +
+    "\n" +
+    "Wenn Sie die vielfältigen Eigenschaften der Berberitze nutzen möchten, sollten Sie die Anwesenheit dieses köstlichen und nahrhaften Stoffes auf Ihrem Tisch begrüßen, indem Sie hochwertige Berberitze auswählen, kaufen und in Ihre Ernährung aufnehmen. Gibt es einen anderen Fotoofen beim Kauf hochwertiger Berberitze, der in diesem Artikel nicht erwähnt wird? Weißt du etwas, was du uns beibringen kannst? Wir warten auf dich."
+}
